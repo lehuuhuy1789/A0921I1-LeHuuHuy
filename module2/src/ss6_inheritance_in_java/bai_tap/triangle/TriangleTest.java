2df32b6cb14ca42bf36e31d73335899c2f7ffb2f
lehuuhuy1789 <87653128+lehuuhuy1789@users.noreply.github.com>
2021-12-17 18:32:18 +0700

    up bai tap tiep

---
+++ module2/src/ss6_inheritance_in_java/bai_tap/triangle/TriangleTest.java
@@ -0,0 +1,8 @@
+package ss6_inheritance_in_java.bai_tap.triangle;
+
+public class TriangleTest {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(5, 4, 3);
+        System.out.println(triangle);
+    }
+}
