df5129a055b7e021f695c860ec49c9e61e18d288
lehuuhuy1789 <87653128+lehuuhuy1789@users.noreply.github.com>
2022-01-18 11:11:05 +0700

    up

---
+++ module2/src/ss14_thuat_toan_sap_xep/bai_tap/MinhHoaThuatToanSapXepChen.java
@@ -0,0 +1,24 @@
+package ss14_thuat_toan_sap_xep.bai_tap;
+
+import java.util.Arrays;
+
+public class MinhHoaThuatToanSapXepChen {
+    public static void main(String[] args) {
+        int[] arr = {12,4,15};
+        int[] arr1 = {1,5,8,4,6,-2,7};
+        insertSort(arr1);
+    }
+    public static void insertSort(int[] array){
+        int pos,x;
+        for (int i = 1; i < array.length; i++) {
+            x=array[i];
+            pos=i;
+            while (pos>0 && x<array[pos-1]) {
+                array[pos] = array[pos-1];
+                pos--;
+            }
+            array[pos]=x;
+            System.out.println(Arrays.toString(array));
+        }
+    }
+}
